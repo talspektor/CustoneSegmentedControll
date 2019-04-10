b2f0f83b2af9460d22cf1dceafd25573971b622c
user140592 <user140592@eu201.local>
2019-04-10 20:08:24 +0100

    Initial Commit

---
+++ CustoneSegmentedControll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustoneSegmentedControll
+//
+//  Created by user140592 on 4/10/19.
+//  Copyright © 2019 talspektor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
